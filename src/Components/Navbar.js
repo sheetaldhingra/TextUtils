e363ba92642034b8452d74751b0c838244f0d15c
sheetaldhingra <129584672+sheetaldhingra@users.noreply.github.com>
2023-04-01 15:38:58 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -10,14 +10,14 @@ export default function Navbar(props) {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <Link className="navbar-brand" to="/">{props.title}</Link>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">{props.homeText}</a>
+          <Link className="nav-link active" aria-current="page" to="/">{props.homeText}</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#" >{props.aboutText}</a>
+          <Link className="nav-link" to="/about" >{props.aboutText}</Link>
         </li>
       </ul>
       {/* <form className="d-flex" role="search">
